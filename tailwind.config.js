ff1141f4d8c83732ff1073c0d6a83cff0b66e73c
Peter Holló <hello@songsforthe.dev>
2024-09-12 09:53:14 +0200

    chore: Use built css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.html"],
+  content: ["./public/**/*.html", "./src/**/*.css"],
   theme: {
     extend: {},
   },
